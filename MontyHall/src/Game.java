8b00f25c75d7c78039048dc83306248d1a31d06a
smgoss <goss.stephenm@gmail.com>
2016-03-13 19:10:04 -0400

    Added simulator
    Added input prompting for running the simulator. Removed an unused file.
    Added comments to Game file.

--- MontyHall/src/Game.java
+++ MontyHall/src/Game.java
@@ -2,118 +2,124 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.List;
 import java.util.Random;
-
+/**
+ * Game.java
+ * @author Stephen Goss
+ * A class to play the Monty Hall problem.
+ */
 public class Game {
+	// The list of doors to pick from.
 	private List<Door> doors;
-	//private int listSize = 0;
+	//The player in this game.
 	private Player player = new Player();
+	//The number of doors in this game.
 	private int size;
-	/*
-	 * Constructor
+	/**
+	 * Constructor - Makes an instance of a game.
 	 * @param size - the number of doors in this game
 	 */
 	public Game(int size){
+		// Sets the size of this game
 		this.size = size;
+		// Initializes the list of doors.
 		doors = new ArrayList<Door>();
 	}
+	/**
+	 * buildGame - Builds the game to be played.
+	 */
 	public void buildGame(){
-		
+		// Random number generator
 		Random r = new Random();
 		int winner = r.nextInt(this.size);
-		//System.out.println("Winner: "+winner);
-		Door winningDoor=new Door(winner+1, Prize.CAR);
+		// Loop through and add all the doors to the list.
 		for(int i = 0; i < this.size; i++){
 			if(i != winner){
+				// Add the Zonks
 				doors.add(i,new Door(i+1,Prize.ZONK));
 			} else {
-				doors.add(i, winningDoor);
-				
+				// Add the winning door
+				doors.add(i, new Door(winner+1, Prize.CAR));
 			}
 		}
-		
 	}
-	private void setPlayerDoor(int door){
+	/**
+	 * setPlayerDoor - Gets the players initial door.
+	 * @param door - the door the player wants
+	 */
+	public void setPlayerDoor(int door){
 		player.setDoor(doors.remove(door-1));
 	}
+	/**
+	 * shuffleDoors - Randomizes the doors.
+	 */
 	public void shuffleDoors(){
+		// Random Number Generator
 		Random r = new Random();
+		// Loop through and swap all the doors around
 	    for (int i = doors.size(); i > 1; i--){
 	        swapDoors( i - 1, r.nextInt(i));
 	    }
 	}
+	/**
+	 * swapDoors - swaps the position of two doors.
+	 * @param door1 - Index of first door to swap with
+	 * @param door2 - Index of second door to swap with
+	 */
 	private void swapDoors(int door1, int door2){
-		Door d1 = doors.get(door1);
-		Door d2 = doors.get(door2);
-		doors.set(door1, d2);
-		doors.set(door2, d1);
+		// Store door 1 before it is over written
+		Door tmp = doors.get(door1);
+		// Replace door 1 with door 2
+		doors.set(door1, doors.get(door2));
+		// Replace door 2 with the saved door 1
+		doors.set(door2, tmp);
 	}
-	public void revealDoors(){
-		int counter = 0;
-		int mysize = doors.size();
+	/**
+	 * revealDoors - Show all but one of the doors. 
+	 */
+	public void revealDoors(boolean silent){
+		int counter = 0;	
+		// Loop through showing each door.
 		while(doors.size() > 1){
-			//System.out.println("Size: "+doors.size());
-			//System.out.println("Counter: " + counter);
+			// Check if there is a car behind the door
 			if (doors.get(counter).getPrize() != Prize.CAR){
+				// Show the door if it is a Zonk
 				Door thedoor = doors.remove(counter);
-				//System.out.println("Door: " + thedoor.getNumber() + " is a " + thedoor.getPrize().toString());
-				//mysize--;
+				if(!silent){
+					System.out.println("Door "+thedoor.getNumber()+"is a "+thedoor.getPrize().toString()+".");
+				}
 			} else {
-				//System.out.println("Found a Car behind door "+doors.get(counter).getNumber());
+				// Don't show this door.
 				counter++;
 			}
 			
 		}	
 	}
-	public boolean checkWinner(){
-		//System.out.println("The player wins a: "+player.getDoor().getPrize().toString());
+	/**
+	 * checkWinner - Check to see if the player won.
+	 * @return - true if we won.
+	 */
+	public boolean checkWinner(boolean silent){
 		if (player.getDoor().getPrize() == Prize.CAR){
+			if(!silent){
+				System.out.println("You won a "+player.getDoor().getPrize().toString()+"!");
+			}
 			return true;
 		} else {
+			if(!silent){
+				System.out.println("You loose and get a "+player.getDoor().getPrize().toString());
+			}
 			return false;
 		}
 		
 	}
-	public void switchDoor(boolean change){
-		if (change){
+	/**
+	 * switchDoor - switch the player door with the first
+	 * 				(and only) door in the queue.
+	 */
+	public void switchPlayerDoor(){
 			Door tmp = doors.remove(0);
 			doors.add(player.getDoor());
 			player.setDoor(tmp);
-			//System.out.println("New Player Door is: "+player.getDoor().getNumber());
-		}
 	}
-	public boolean playGame(boolean doorChange){
-		Random r = new Random();
-		this.buildGame();
-		this.setPlayerDoor(r.nextInt(this.size)+1);
-		this.shuffleDoors();
-		this.revealDoors();
-		this.switchDoor(doorChange);
-		return this.checkWinner();
-	}
-
-	/*public static void main(String[] args){
-		double wins = 0;
-		for( int i = 0; i < 1000000; i ++){
-			Game theGame = new Game(100);
-			//System.out.print("What number would you like?: ");
-			Scanner in = new Scanner(System.in);
-			//int num = in.nextInt();
-			Random r = new Random();
-			theGame.buildGame();
-			theGame.setPlayerDoor(r.nextInt(3)+1);
-			theGame.shuffleDoors();
-			theGame.revealDoors();
-			theGame.switchDoor(true);
-			
-			if (theGame.checkWinner()){
-				wins++;
-			}
-		}
-		double percent = ( wins / 1000000 ) * 100;
-		System.out.println("Wins = "+wins);
-		System.out.println("Percent: " + percent);
-		*/
-		
-	//}
 }
 
--- MontyHall/src/Play.java
+++ MontyHall/src/Play.java
@@ -1,17 +1,52 @@
-
+import java.util.Random;
+import java.util.Scanner;
 public class Play {
 	public Play(){
 	}
-	public static void main(String[] args){
+	public void simulator(int numDoors, int trials, boolean playerChoice){
 		double wins = 0;
-		int trials = 1000000;
+		Random r = new Random();
 		for (int i = 0; i < trials; i++ ){
-			Game theGame = new Game(4);
-			if (theGame.playGame(true)){
+			Game theGame = new Game(numDoors);
+			theGame.buildGame();
+			theGame.setPlayerDoor(r.nextInt(numDoors)+1);
+			theGame.shuffleDoors();
+			theGame.revealDoors(true);
+			if(playerChoice){
+				theGame.switchPlayerDoor();
+			}
+			if(theGame.checkWinner(false)){
 				wins++;
 			}
 		}
 		double percent= (wins/trials)*100;
 		System.out.println("Percentage won: "+ percent);
+	}
+	public static void main(String[] args){	
+		int numDoors = 0;
+		int numTrials = 0;
+		int playerChoice = 0;
+		try{
+			System.out.print("How many doors would you like to play with? ");
+			Scanner s1 = new Scanner(System.in);
+			numDoors = s1.nextInt();
+			System.out.print("How many trials would you like to run? ");
+			numTrials = s1.nextInt();
+			System.out.print("Would you like to say or switch?\n[1] Stay\n[2] Switch\n:");
+			playerChoice = s1.nextInt();
+			if ((playerChoice != 1) && (playerChoice!= 2)){
+				System.out.println("Bad Input!");
+				System.exit(-1);
+			}
+		} catch (Exception e){
+			System.out.println("Bad input entered!");
+			System.exit(-1);
+		}
+		Play p1 = new Play();
+		if (playerChoice == 1){
+			p1.simulator(numDoors, numTrials, false);
+		} else {
+			p1.simulator(numDoors, numTrials, true);
+		}
 	}
 }
